922108ac288cf3baa9b58c7bc1f10a6362e513cd
devadh21 <devadh21@gmail.com>
2024-10-06 11:59:31 +0000

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
 import { getProviders, signIn } from "next-auth/react";
 import { useEffect,useState } from "react";
 
